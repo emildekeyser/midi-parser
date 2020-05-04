46739d1f744435bdfabe2e3b4b2a4812c069fef7
emil <emil.dekeyser@student.ucll.be>
2020-05-04 13:38:21 +0200

    primitives
    02-midi/01-primitives/03-instruments
    02-midi/01-primitives/04-note-numbers

--- src/midi/midi/primitives.h
+++ src/midi/midi/primitives.h
@@ -7,8 +7,23 @@
 namespace midi
 {
     struct Channel
-        : tagged<uint8_t, Channel>, equality<Channel>, show_value<Channel, int>
+        : tagged<uint8_t, Channel>,
+        equality<Channel>,
+        show_value<Channel, int>
         { using tagged::tagged; };
+
+    struct Instrument
+        : tagged<uint8_t, Instrument>,
+        equality<Instrument>,
+        show_value<Instrument, int>
+        { using tagged::tagged; };
+
+    struct NoteNumber
+        : tagged<uint8_t, NoteNumber>,
+        ordered<NoteNumber>,
+        show_value<NoteNumber, int>
+        { using tagged::tagged; };
+
 }
 
 #endif
